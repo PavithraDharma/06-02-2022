953d6c17692b780247672c06641ef72309f95cd4
Pavithra <Pavithra@LAPTOP-ECJUFKE0>
2022-02-13 17:28:35 +0530

    06Feb Commit

---
+++ 06-02-2022/src/com/demo/Leap.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+class Leap {
+	public static void main(String[] args) {
+		int year=1998;
+		if(year%4==0)
+		{
+			System.out.println("Its a leap year");
+		}
+		else
+		{
+			System.out.println("Its not a leap year");
+		}
+	}
+}
